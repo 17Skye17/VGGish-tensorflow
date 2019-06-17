251cc7dbcf624775c6e89938d89f6ce179b437fd
pytlab <shaozhengjiang@gmail.com>
2019-06-17 13:03:21 +0800

    fix

--- concate_features.sh
+++
@@ -1,1 +0,0 @@
-python concate_features.py /DATACENTER/1/skye/YingYongBao/raw.hdf5 /DATACENTER/1/skye/YingYongBao/vggish.hdf5 /DATACENTER/1/skye/YingYongBao/resnet101-vggish-user.hdf5 /DATACENTER/1/skye/YingYongBao/inceptionv3-hdf5/$1
